fc79037a8ba3f2c91a7bd35eeab70e5f3fe3bb23
YuxuanQiu <1090259102@qq.com>
2022-11-29 21:02:02 -0500

    updates

--- full-stack-developer-server-node/app.js
+++ full-stack-developer-server-node/app.js
@@ -3,8 +3,12 @@ import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js"
 import mongoose from "mongoose"
-mongoose.connect('mongodb://localhost:27017/tuiter')
 import cors from "cors";
+// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+//     || 'mongodb://localhost:27017/tuiter'
+// mongoose.connect(CONNECTION_STRING);
+// mongoose.connect('mongodb://localhost:27017/tuitermongodb+srv://root:<xuan1234>@cluster0.m81cqqo.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://root:root12345678@cluster0.y77jd0u.mongodb.net/?retryWrites=true&w=majority')
 const app = express();
 app.use(cors());
 app.use(express.json());
